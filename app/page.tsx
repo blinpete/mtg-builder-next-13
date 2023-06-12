ce7f28a7a88bde6e5229021f51037c30ed2b79f0
Peter <pete.blinov@yandex.ru>
2023-06-12 22:15:35 +0300

    chore: clean

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ export default function Home() {
   return (
     <div className="text-center">
       Welcome to
-      02934r9823h402893hr
       <h1 className="text-2xl">MTG Builder</h1>
     </div>
   )
