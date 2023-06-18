8c267e3ed39a8c522ad2179c097ed3930202451f
Peter <pete.blinov@yandex.ru>
2023-06-18 18:28:47 +0300

    fix: `ThemeProvider` needs `suppressHydrationWarning` attribute

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={cn(font.className, 'overscroll-y-none')}>
         {/* header */}
         <div className="
