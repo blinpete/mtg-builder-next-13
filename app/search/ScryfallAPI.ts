4cb105630aa451eb87d67524401a500a12ffdb52
Peter <pete.blinov@yandex.ru>
2023-06-22 23:29:28 +0300

    chore: refactor ScryfallAPI

--- app/search/ScryfallAPI.ts
+++ app/search/ScryfallAPI.ts
@@ -10,6 +10,13 @@ export type ScrySearchResponse = {
   data: Scry.Card[]
 }
 
+export type ScrySearchError = {
+  code: string
+  details: string
+  object: "error"
+  status: number
+}
+
 /**
  * Docs:
  * https://scryfall.com/docs/api/lists
@@ -23,8 +30,12 @@ export async function ScrySearch(query: string, options?: Scry.SearchOptions) {
     `q=${query}`
   )
 
+  // await new Promise(resolve => setTimeout(resolve, 1500))
+
   const response = await fetch(`https://api.scryfall.com/cards/search?${searchParams}`)
-  const json = (await response.json()) as ScrySearchResponse
+  const json = (await response.json()) as ScrySearchResponse | ScrySearchError
+
+  if (json.object === "error") throw json
 
   return json
 }
