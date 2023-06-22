9015df0dc2fca729b474f59a6e5223323fe36d56
Peter <pete.blinov@yandex.ru>
2023-06-22 23:29:41 +0300

    WIP DeckContext

---
+++ app/search/DeckContext.ts
@@ -0,0 +1,12 @@
+// https://react.dev/learn/scaling-up-with-reducer-and-context
+
+import { createContext } from "react";
+import { type Scry } from "./ScryfallAPI";
+
+export const DeckContext = createContext<{
+  cards: Scry.Card[]
+  addCard: (card: Scry.Card) => void
+}>({
+  cards: [],
+  addCard: null,
+})
