34c4a6b4e138b250fafec669742b788a1b6b85aa
Peter <pete.blinov@yandex.ru>
2023-06-23 15:38:07 +0300

    chore: run `nr fmt`

--- app/search/DeckContext.ts
+++ app/search/DeckContext.ts
@@ -1,7 +1,7 @@
 // https://react.dev/learn/scaling-up-with-reducer-and-context
 
-import { createContext } from "react";
-import { type Scry } from "./ScryfallAPI";
+import { createContext } from "react"
+import { type Scry } from "./ScryfallAPI"
 
 export const DeckContext = createContext<{
   cards: Scry.Card[]
