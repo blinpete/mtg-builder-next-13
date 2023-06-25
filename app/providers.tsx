f2e0070f29358f8043ddddf8f5d795178df30866
Peter <pete.blinov@yandex.ru>
2023-06-25 10:14:24 +0300

    chore: set `react-query` defaults to not refetch every damn second

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,7 +5,15 @@ import { type PropsWithChildren } from "react"
 import { QueryClient, QueryClientProvider } from "react-query"
 import { DeckProvider } from "./search/DeckContext"
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+    },
+  },
+})
 
 export function Providers({ children }: PropsWithChildren) {
   return (
