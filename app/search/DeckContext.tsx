7f8d6d37a0768a495d67afb4599c5fc93ae550f4
Peter <pete.blinov@yandex.ru>
2023-06-30 21:22:40 +0300

    WIP DeckContext (before refactor)

--- app/search/DeckContext.tsx
+++ app/search/DeckContext.tsx
@@ -1,20 +1,23 @@
 // https://react.dev/learn/scaling-up-with-reducer-and-context
 
-import { createContext, useContext, useState } from "react"
+import { createContext, useContext, useMemo, useState } from "react"
 import { useMutation, useQuery, useQueryClient } from "react-query"
 import { useDeckQuery } from "../decks/[id]/useDeckQuery"
 import { type Scry } from "./ScryfallAPI"
 import type { DeckRecordLoaded } from "../api/deck/decks-json"
 import type { PropsWithChildren } from "react"
+import type { Card } from "scryfall-sdk"
+
+type CardEntry = {
+  card: Scry.Card
+  count: number
+}
 
 export type DeckLocal = {
   id: string
   name: string
+  cards: CardEntry[]
 
-  cards: {
-    card: Scry.Card
-    count: number
-  }[]
   has: (id: string) => boolean
   addCard: (card: Scry.Card) => void
   removeCard: (id: Scry.Card["id"]) => void
@@ -31,61 +34,76 @@ export function DeckProvider({ children }: PropsWithChildren) {
   const { data: deckServer, error, isFetching } = useDeckQuery({ id: deckId })
   console.log("🚀 | DeckProvider | deckServer:", deckServer)
 
-  const queryClient = useQueryClient()
+  const [added, setAdded] = useState<Record<string, CardEntry>>({})
+  const [updated, setUpdated] = useState<Record<string, number>>({})
+
+  const addCard = (card: Card) => {
+    if (!deckServer) return
+
+    const idx = deckServer.cards.findIndex(x => x.card.id === card.id)
+    if (idx !== -1) {
+      setUpdated(prev => ({
+        ...prev,
+        [card.id]: (prev[card.id] || 0) + 1,
+      }))
+      return
+    }
+
+    setAdded(prev => ({
+      ...prev,
+      [card.id]: { card, count: (prev[card.id]?.count || 0) + 1 },
+    }))
+  }
+
+  const removeCard = (id: string) => {
+    if (!deckServer) return
+
+    if (added[id]) {
+      added[id].count -= 1
+      setAdded(added)
+
+      if (added[id].count <= 0) {
+        delete added[id]
+        setAdded({ ...added })
+      }
+      return
+    }
+
+    setUpdated(prev => ({
+      ...prev,
+      [id]: (prev[id] || 0) - 1,
+    }))
+  }
+
+  const cards = useMemo(() => {
+    if (!deckServer) return []
+
+    const cards = deckServer?.cards.map(x => {
+      const count = x.count + (updated[x.card.id] || 0)
+      if (count <= 0) return null
+
+      return { card: x.card, count }
+    })
+
+    for (const k in added) {
+      cards.push(added[k])
+    }
+
+    return cards?.filter(Boolean) as CardEntry[]
+  }, [deckServer, added, updated])
+
+  const has = (id: string) => !!cards.find(x => x.card.id === id)
 
   let deck: DeckLocal | null = null
-  if (deckServer)
+  if (!deckServer) {
     deck = {
-      ...deckServer,
-
-      addCard: card => {
-        queryClient.setQueryData<DeckRecordLoaded | null>(["deck", deckId], oldData => {
-          if (!oldData) {
-            console.log("🚀 | addCard: no oldData!")
-            return null
-          }
-          const cards = [...oldData.cards]
-          const found = cards.find(x => x.card.id === card.id)
-          if (found) {
-            if (found.count >= 4)
-              console.warn(
-                `Warning: Adding "${card.name}" might be a mistake. There are already ${found.count} copies of the card in the deck.`
-              )
-            found.count += 1
-          } else {
-            cards.push({ card, count: 1 })
-          }
-
-          const newData = { ...oldData, cards }
-          return newData
-        })
-      },
-
-      removeCard: id => {
-        queryClient.setQueryData<DeckRecordLoaded | null>(["deck", deckId], oldData => {
-          if (!oldData) {
-            console.log("🚀 | removeCard: no oldData!")
-            return null
-          }
-
-          const cards = [...oldData.cards]
-          const idx = cards.findIndex(x => x.card.id === card.id)
-          if (idx === -1) return oldData
-
-          if (cards[idx].count <= 0) {
-            console.warn(
-              `Warning: Removing "${cards[idx].card.name}" might be a mistake. There are already ${cards[idx].count} copies of the card in the deck.`
-            )
-          }
-          cards[idx].count -= 1
-
-          const newData = { ...oldData, cards }
-          return newData
-        })
-      },
-
-      has: id => !!cards[id]?.count,
+      ...(deckServer || {}),
+      cards,
+      addCard,
+      removeCard,
+      has,
     }
+  }
 
   return <DeckContext.Provider value={{ deck, setDeckId }}>{children}</DeckContext.Provider>
 }
