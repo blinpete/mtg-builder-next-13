8b1b6224c5bc99572e8f44756304b0e91928818d
Peter <pete.blinov@yandex.ru>
2023-07-01 00:16:42 +0300

    chore(refactor): separate SearchForm

---
+++ app/search/SearchForm.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import { useEffect, useRef } from "react"
+import type { FormEventHandler } from "react"
+
+type Props = {
+  query: string | null
+  onSubmit: FormEventHandler<HTMLFormElement>
+}
+export function SearchForm(props: Props) {
+  console.log("🚀 | SearchForm")
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.value = props.query || ""
+    }
+  }, [props.query])
+
+  return (
+    <>
+      <form onSubmit={props.onSubmit} className="w-full px-5 my-2">
+        <label>
+          <input
+            ref={inputRef}
+            name="search"
+            className="
+              px-2 py-1.5 outline-none rounded-md
+              border-2 border-transparent
+              focus:border-slate-500
+              bg-zinc-400 text-gray-800
+              w-full
+            "
+          ></input>
+        </label>
+      </form>
+    </>
+  )
+}
