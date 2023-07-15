08df2b71e43dcb18d1179c1c078efaa02b938cc1
Peter <pete.blinov@yandex.ru>
2023-07-15 23:56:35 +0300

    fix: update /decks page, add "New deck" btn

--- app/decks/page.tsx
+++ app/decks/page.tsx
@@ -1,8 +1,9 @@
 "use client"
 
 import Link from "next/link"
-import { usePathname } from "next/navigation"
-import { useQuery } from "react-query"
+import { usePathname, useRouter } from "next/navigation"
+import { useQuery, useQueryClient } from "react-query"
+import { useDeck } from "../search/DeckContext"
 import { DeckCover } from "./DeckCover"
 import type { DeckRecord } from "../api/deck/decks-json"
 
@@ -23,13 +24,49 @@ export default function DeckPage() {
   console.log("🚀 | DeckPage | error:", error)
   console.log("🚀 | decks:", decks)
 
+  const queryClient = useQueryClient()
+  const router = useRouter()
+  const { setDeckId } = useDeck()
+
+  async function addDeck() {
+    const response = await fetch("http://localhost:3000/api/deck/new", {
+      method: "POST",
+      cache: "no-cache",
+    })
+    console.log("🚀 | addDeck | response:", response)
+
+    if (response.ok) {
+      const decks = (await response.json()) as DeckRecord[]
+      console.log("🚀 | addDeck | decks:", decks)
+
+      queryClient.setQueryData(["decks"], decks)
+
+      setDeckId(decks[decks.length - 1].id)
+      router.push("/decks/edit")
+    }
+  }
+
   return (
     <section>
       {error && <div>Error: {JSON.stringify(error)}</div>}
 
-      {decks && (
-        <ul className="flex">
-          {decks.map(d => (
+      <ul className="flex">
+        <li key="decks_new" className="cursor-pointer hover:opacity-70">
+          <div onClick={addDeck} className="flex items-center p-2">
+            <div
+              className="flex items-center justify-center
+                w-24 h-32
+                border-2 border-gray-400 border-dashed rounded-md
+                bg-gray-400/30 text-gray-600/70
+                text-sm
+              "
+            >
+              New deck
+            </div>
+          </div>
+        </li>
+        {decks &&
+          decks.map(d => (
             <li key={"decks_" + d.id} className="cursor-pointer hover:opacity-90">
               <Link href={pathname + "/" + d.id} className="flex flex-col items-center p-2">
                 <DeckCover deck={d} />
@@ -41,8 +78,7 @@ export default function DeckPage() {
               </Link>
             </li>
           ))}
-        </ul>
-      )}
+      </ul>
     </section>
   )
 }
