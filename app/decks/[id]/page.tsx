14875a59c25532fe5a8fc42556b186a3285e6820
Peter <pete.blinov@yandex.ru>
2023-07-23 21:18:40 +0300

    WIP after merge

--- app/decks/[id]/page.tsx
+++ app/decks/[id]/page.tsx
@@ -1,13 +1,24 @@
 "use client"
 
 import { useParams, useRouter } from "next/navigation"
+import { useSession } from "next-auth/react"
 import { useMemo } from "react"
 import { CardsGrid } from "@/app/search/CardsGrid"
 import { useDeck } from "@/app/search/DeckContext"
 import { CardDotCounter } from "../edit/CardDotCounter"
 import { useDeckQuery } from "./useDeckQuery"
+import type { Prisma } from "@prisma/client"
 
-export default function DeckPage() {
+export default function DeckPage({ params }: { params: { name: string } }) {
+  const { status } = useSession()
+
+  if (status === "loading") return <p>Loading...</p>
+  if (status === "unauthenticated") return <p>You are not logged in</p>
+
+  return <Deck />
+}
+
+function Deck() {
   const params = useParams()
   console.log("🚀 | DeckPage | params:", params)
 
@@ -23,7 +34,7 @@ export default function DeckPage() {
   const router = useRouter()
   const { setDeckId } = useDeck()
 
-  if (isFetching) return <div>Loading...</div>
+  if (isFetching) return <div>Loading the deck...</div>
 
   if (!deck) return <div>No deck found by id: {params.id}</div>
   // if (!deck.cards.length) return <div>Empty deck</div>
@@ -37,6 +48,11 @@ export default function DeckPage() {
     <section>
       {deck && (
         <div className="w-full mx-1 my-3 flex justify-between items-center">
+          <p>Deck name: {deck?.name}</p>
+          <p>Cards: {deck?.cards.length}</p>
+          <p>Sideboard: {deck?.sideboard}</p>
+          <p>Created at: {JSON.stringify(deck?.createdAt)}</p>
+
           <div className="font-semibold">
             {deck.name}: {deck.cards.length} cards
           </div>
