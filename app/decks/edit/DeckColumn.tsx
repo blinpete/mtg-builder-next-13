647690868a345f51e744ec30b4a8eab36ec18c7e
Peter <pete.blinov@yandex.ru>
2023-08-06 12:43:16 +0300

    chore: refactor DeckColumn, add Heading to separate deck/sideboard/champions

--- app/decks/edit/DeckColumn.tsx
+++ app/decks/edit/DeckColumn.tsx
@@ -5,6 +5,16 @@ import type { SetActiveCardAction } from "./page"
 import type { DeckLocal } from "@/types/decks"
 import type { Card } from "scryfall-sdk"
 
+function DeckSectionHeading({ title }: { title: string }) {
+  return (
+    <h1 className="text-xs flex gap-2 pl-3 pr-4 items-center text-zinc-600/60">
+      <hr className="flex-grow border-zinc-600/30" />
+      <span>{title}</span>
+      <hr className="flex-grow border-zinc-600/30" />
+    </h1>
+  )
+}
+
 export function DeckColumn({
   deck,
   activeCard,
@@ -15,49 +25,52 @@ export function DeckColumn({
   setActiveCard: SetActiveCardAction
 }) {
   return (
-    <ul
-      className="
+    <div>
+      <DeckSectionHeading title="Deck" />
+      <ul
+        className="
         mx-1 mt-4 mb-2 list-none
         grid gap-y-1 grid-cols-1
       "
-    >
-      {deck.cards.map(({ card, count }, i) => (
-        <CardCompact
-          key={card.id + i}
-          card={card}
-          onClick={() => setActiveCard(prev => (prev?.id === card.id ? null : card))}
-          slotLeftCorner={<span>{count}</span>}
-          slotControls={
-            activeCard?.id !== card.id ? null : (
-              <div
-                className="
+      >
+        {deck.cards.map(({ card, count }, i) => (
+          <CardCompact
+            key={card.id + i}
+            card={card}
+            onClick={() => setActiveCard(prev => (prev?.id === card.id ? null : card))}
+            slotLeftCorner={<span>{count}</span>}
+            slotControls={
+              activeCard?.id !== card.id ? null : (
+                <div
+                  className="
                   absolute top-0 bottom-1 left-1 right-2
                   rounded-full pr-10
                   flex gap-0.5 justify-end
                 "
-                style={{
-                  background: "radial-gradient(rgba(0 0 0 / 10%) 10%, black)",
-                }}
-              >
-                <CardCompactButton
-                  onClick={() => {
-                    deck.removeCard(card.id)
+                  style={{
+                    background: "radial-gradient(rgba(0 0 0 / 10%) 10%, black)",
                   }}
                 >
-                  -
-                </CardCompactButton>
-                <CardCompactButton
-                  onClick={() => {
-                    deck.addCard(card)
-                  }}
-                >
-                  +
-                </CardCompactButton>
-              </div>
-            )
-          }
-        />
-      ))}
-    </ul>
+                  <CardCompactButton
+                    onClick={() => {
+                      deck.removeCard(card.id)
+                    }}
+                  >
+                    -
+                  </CardCompactButton>
+                  <CardCompactButton
+                    onClick={() => {
+                      deck.addCard(card)
+                    }}
+                  >
+                    +
+                  </CardCompactButton>
+                </div>
+              )
+            }
+          />
+        ))}
+      </ul>
+    </div>
   )
 }
