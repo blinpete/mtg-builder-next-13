c8165c2fc6cd7b1ff128982a4b5945ef12b50816
Peter <pete.blinov@yandex.ru>
2023-08-06 13:38:50 +0300

    chore: refactor DeckColumn, add CardControlsPlusMinus component
    sideboard controls will override those

--- app/decks/edit/DeckColumn.tsx
+++ app/decks/edit/DeckColumn.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image"
-import { cn } from "@/lib/utils"
 import { CardCompact, CardCompactButton } from "./CardCompact"
 import type { SetActiveCardAction } from "./page"
 import type { DeckLocal } from "@/types/decks"
@@ -15,6 +13,36 @@ function DeckSectionHeading({ title }: { title: string }) {
   )
 }
 
+function CardControlsPlusMinus({ deck, card }: { deck: DeckLocal; card: Card }) {
+  return (
+    <div
+      className="
+        absolute top-0 bottom-1 left-1 right-2
+        rounded-full pr-10
+        flex gap-0.5 justify-end
+      "
+      style={{
+        background: "radial-gradient(rgba(0 0 0 / 10%) 10%, black)",
+      }}
+    >
+      <CardCompactButton
+        onClick={() => {
+          deck.removeCard(card.id)
+        }}
+      >
+        -
+      </CardCompactButton>
+      <CardCompactButton
+        onClick={() => {
+          deck.addCard(card)
+        }}
+      >
+        +
+      </CardCompactButton>
+    </div>
+  )
+}
+
 export function DeckColumn({
   deck,
   activeCard,
@@ -26,6 +54,33 @@ export function DeckColumn({
 }) {
   return (
     <div>
+      <DeckSectionHeading title="Champions" />
+      <ul
+        className="
+        mx-1 mt-4 mb-2 list-none
+        grid gap-y-1 grid-cols-1
+      "
+      >
+        {deck.champions.map(({ id }, i) => {
+          const item = deck.cards.find(x => x.card.id === id)
+          if (!item) return "not in deck"
+
+          return (
+            <CardCompact
+              key={id + "__champion__" + i}
+              card={item.card}
+              onClick={() => setActiveCard(prev => (prev?.id === item.card.id ? null : item.card))}
+              slotLeftCorner={<span>{item.count}</span>}
+              slotControls={
+                activeCard?.id !== item.card.id ? null : (
+                  <CardControlsPlusMinus deck={deck} card={item.card} />
+                )
+              }
+            />
+          )
+        })}
+      </ul>
+
       <DeckSectionHeading title="Deck" />
       <ul
         className="
@@ -40,33 +95,7 @@ export function DeckColumn({
             onClick={() => setActiveCard(prev => (prev?.id === card.id ? null : card))}
             slotLeftCorner={<span>{count}</span>}
             slotControls={
-              activeCard?.id !== card.id ? null : (
-                <div
-                  className="
-                  absolute top-0 bottom-1 left-1 right-2
-                  rounded-full pr-10
-                  flex gap-0.5 justify-end
-                "
-                  style={{
-                    background: "radial-gradient(rgba(0 0 0 / 10%) 10%, black)",
-                  }}
-                >
-                  <CardCompactButton
-                    onClick={() => {
-                      deck.removeCard(card.id)
-                    }}
-                  >
-                    -
-                  </CardCompactButton>
-                  <CardCompactButton
-                    onClick={() => {
-                      deck.addCard(card)
-                    }}
-                  >
-                    +
-                  </CardCompactButton>
-                </div>
-              )
+              activeCard?.id !== card.id ? null : <CardControlsPlusMinus deck={deck} card={card} />
             }
           />
         ))}
