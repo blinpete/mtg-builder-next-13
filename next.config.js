baa3fb0fb30f01f1a5977a09cf7027776268bfb8
Peter <pete.blinov@yandex.ru>
2023-09-01 13:14:03 +0300

    add `webpack-dev-server-qrcode`

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const WebpackQRCodePlugin = require("webpack-dev-server-qr-code")
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -7,6 +9,16 @@ const nextConfig = {
   experimental: {
     // turbo: {},
     // typedRoutes: true,
+  },
+
+  webpack: (config, options) => {
+    config.devServer = {
+      port: 3000,
+      host: "0.0.0.0",
+    }
+
+    config.plugins = [...(config.plugins || []), new WebpackQRCodePlugin({ size: "small" })]
+    return config
   },
 }
 
