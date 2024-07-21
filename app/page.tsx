030c05b228421792dac049d43a246a0fdc2a2137
Peter <pete.blinov@yandex.ru>
2024-07-21 16:31:25 +0300

    extract page `HomePage`

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,5 @@
-// import { getServerSession } from "next-auth"
-// import User from "@/components/User"
-// import { authOptions } from "./api/auth/[...nextauth]/route"
+import { HomePage } from "@pages/HomePage"
 
 export default async function Home() {
-  // const session = await getServerSession(authOptions)
-
-  return (
-    <div
-      className="flex-grow flex flex-col justify-center text-center bg-clip-text"
-      style={{
-        opacity: 0.75,
-        color: "transparent",
-        textShadow: "rgb(236 230 230 / 50%) 2px 2px 3px",
-        backgroundColor: "rgb(147 155 161)",
-        fontFamily: "system-ui",
-      }}
-    >
-      <span className="text-md font-semibold">Welcome to</span>
-      <h1 className="text-4xl font-extrabold">MTG Builder</h1>
-      {/* <h2>You are logged as:</h2>
-      <p>SSR session info:</p>
-      <pre>{JSON.stringify(session)}</pre>
-      <p>Client side rendered session info:</p>
-      <User />
-      <br />
-      <a href="/auth/protected" className="text-blue-400 hover:opacity-50 transition">
-        Auth protected page
-      </a> */}
-    </div>
-  )
+  return <HomePage />
 }
